6a8db7c2de65c7511f5883377eb861bb2026b7dc
Zach LeClaire <zacharyjleclaire@gmail.com>
2020-08-07 14:55:29 -0700

    Added code to handle invalid input

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.once('ready', () => {
     //lines.forEach(element => console.log(element));
     for(var i = 0; i < lines.length/2-1; i ++){
       console.log("id#" + lines[2*i+1] + "; worms: " + lines[2*i+2]);
-      setWorm(lines[2*i+1], lines[2*i+2])
+      setWorm(lines[2*i+1], lines[2*i+2]);
     }
     console.log("all worms loaded from " + wormlistpath);
 
@@ -28,7 +28,7 @@ client.once('ready', () => {
 var updateFile = new cronJob("*/10 * * * * *", function(){
     var list = wormList.keyArray();
     list.forEach(element => function(){
-      if(element === null){
+      if(isUser(element)){
         wormList.delete(element);
       }
     });
@@ -36,29 +36,51 @@ var updateFile = new cronJob("*/10 * * * * *", function(){
       if (err) {
         return console.log(err);
       }
-      console.log();
     });
     const d = new Date();
     console.log("logged @", d);
 })
 
+function parseID(id){
+  if(!isNaN(id)){
+    console.log(id + "Skipped bc valid");
+    return id;
+  }
+  try{
+  let parseids = id.split(/[^0-9|,]/);
+  return parseids[3];
+  }
+  catch(err){
+    console.error(err);
+    return null;
+  }
+}
+
+function newLine(){
+  console.log("-------------------------------");
+}
+
 function setWorm(id, worms, message){
-  if(!isNaN(worms)){
+  let parsedID = parseID(id);
+  if(!isNaN(worms)&& isUser(parsedID)){
     try{
-      wormList.set(id, worms);
+      wormList.set(parsedID, worms);
     }
     catch(err){
       console.error(err);
       message.channel.send("Invalid command usage. Please check the guide on using commands");
     }
   }
+  else{
+    message.channel.send("Invalid command usage. Please check the guide on using commands");
+  }
 }
 
-
 function setWorm(id, worms){
-  if(!isNaN(worms)){
+  let parsedID = parseID(id);
+  if(!isNaN(worms)&& isUser(parsedID)){
     try{
-      wormList.set(id, worms)
+      wormList.set(parsedID, worms);
     }
     catch(err){
       console.log("Invalid id");
@@ -67,25 +89,32 @@ function setWorm(id, worms){
 }
 
 function displayWorms(id, channel){
-  var worms = getWorms(id);
-  if(wormList.get(id) > 0){
-    channel.send(id + " has +" + wormList.get(id) + " worm");  
+  let parsedID = parseID(id);
+  var worms = getWorms(parsedID);
+  if(wormList.get(parsedID) > 0){
+    channel.send(id + " has +" + worms + " worm");  
   }
   else{
-    channel.send(id + " has " + wormList.get(id) + " worm");  
+    channel.send(id + " has " + worms + " worm");  
   }
 }
 
 function getWorms(id){
-  let worms = wormList.get(id);
-  if(!isNaN(worms)){
-    return worms;
+  let parsedID = parseID(id);
+  let worms = wormList.get(parsedID);
+  if(!isNaN(worms)&&isUser(parsedID)){
+      return worms;
   }
   else{
-    if(id != 'undefined'){
-      wormList.set(id, 0);
+    if(isUser(parsedID)){
+      console.log('User ' + id + ' record initialized');
+      wormList.set(parsedID, 0);
+      return 0;
     }
-    return NaN;
+    else{
+      console.log("User "+ id + " does not exist");
+      return NaN;
+    }
   }
 }
 
@@ -93,22 +122,30 @@ function displayAll(){
   wormList.forEach(element => console.log(element));
 }
 
-let getNumUsers = function(message, id){
-  var num;
+function isUser(id){
+  if(id != null){
+    return true;
+  }
+  else{
+    return false;
+  }
+  /*
   message.guild.members.fetch().then(fetchedMembers => {
-      num = fetchedMembers.filter(member => member.id == id).size;
-      console.log(num);
-      return num;
+      fetchedMembers.forEach(member => function(){
+        if(member.id == id){
+          return true;
+        }
+      });
+      return false;
    });
-
- //  console.log(num);
-  // return num;
+  */ 
 }
 
 client.on('message', message => {
     // First we use guild.members.fetch to make sure all members are cached
     
-    console.log("value: " + getNumUsers(message, message.author.id));   /* if(getNumUsers(message, message.author.id) == 1){
+    //console.log("value: " + isUser(message, message.author.id));
+    /* if(getNumUsers(message, message.author.id) == 1){
       message.channel.send("You da person");
     }
     
@@ -121,7 +158,7 @@ client.on('message', message => {
         var msg = message.content.split(' ');
         let wormCount = parseInt(msg[1], 10);
         var id = msg[2];
-        var factor = 0;
+        var factor;
         if(!isNaN(wormCount)){
           if(message.content.startsWith(`${prefixSub}`)){
             factor = -1;
@@ -131,27 +168,34 @@ client.on('message', message => {
             factor = 1;
             message.channel.send(id + " has gained " + wormCount + " worms.");
           }
+          setWorm(id, parseInt(getWorms(id)) + factor*wormCount, message);
+          displayWorms(id, message.channel);
         }
         else{
           message.channel.send(message.author.tag + " invalid number of worms. Please try again");
         }
-        getWorms(id);
-        setWorm(id, wormList.get(id) + factor*wormCount, message);
-        displayWorms(id, message.channel);
+        //getWorms(id);
     }
     if(message.content.startsWith(`${prefixSet}`)){
       var msg = message.content.split(' ');
-      var wormCount = msg[1];
+      var wormCount = parseInt(msg[1],10);
       var id = msg[2];
-      setWorm(id, wormCount, message);
-      displayWorms(id, message.channel);
+      if(isUser(id)){
+        setWorm(id, wormCount, message);
+        displayWorms(id, message.channel);
+      }
+      else{
+        message.channel.send(message.author.tag + " invlaid user ID. Please tell me who you are talking about");
+      }
     }
     if(message.content.startsWith(`${prefixGet}`)){
       var msg = message.content.split(' ');
       var id = msg[1];
-      if(id === null){
+      ///var parsedID = parseID(id);
+
+      if(id == null){
         displayAll();
-        message.channel.send(message.author.tag + " invlaid user ID. Please tell me who you are talking about");
+        message.channel.send(message.author.tag + " invalid user ID. Please tell me who you are talking abouokt");
       }
       else{
         displayWorms(id, message.channel);
